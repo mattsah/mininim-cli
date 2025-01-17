dd9ef4fea09d982aa8b83dd6505d57cd9673dbbb
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2025-01-17 02:59:10 -0800

    Functional-ish command facets

--- src/mininim/cli.nim
+++ src/mininim/cli.nim
@@ -1,16 +1,23 @@
 import
     mininim,
-    std/marshal,
     mininim/dic
+
+type
+    CommandHook = proc(app: var App): int {. cdecl  .}
 
 class Command of Facet:
     var
         name*: string
         description*: string
 
+    method execute(app: var App): int
+
 shape Command: @[
     Hook(
-        call: "execute"
+        call: proc(app: var App): int =
+            let command = app.get(Command)
+
+            result = command.execute(app)
     )
 ]
 
@@ -18,17 +25,22 @@ class Console:
     var
         app: App
 
+    method build*(app: var App): Console {. static .} =
+        result = Console.init(app)
+
     method init*(app: var App): void =
         this.app = app
 
     method run*(): int =
-        let command = this.app.config.findAll(Command)
-
         result = 0
 
+        let commands = this.app.config.findAll(Command)
+
+        for command in commands:
+            if command.name == "":
+                result = cast[CommandHook](command.hook)(this.app)
+
+
 shape Console: @[
-    Delegate(
-        builder: proc(app: var App): Console =
-            result = Console.init(app)
-    )
+    Delegate()
 ]
