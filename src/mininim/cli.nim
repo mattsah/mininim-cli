651172305ce9420f58a3d4f30fbc7c839e3a3289
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2025-01-19 15:51:47 -0800

    Class rework

--- src/mininim/cli.nim
+++ src/mininim/cli.nim
@@ -7,50 +7,42 @@ export
     dic
 
 type
-    CommandHook = proc(app: var App): int {. cdecl .}
-    CommandConcept* = concept x
-        x.execute(var App) is int
-
-#[
-## Command
-]#
-class Command of Facet:
-    var
+    Command* = ref object of Facet
         name*: string
         description*: string
 
-    method execute(app: var App): int
+    Console* = ref object of Class
+        app*: App
+        args*: seq[string]
+        opts*: Table[string, string]
+
+    CommandHook = proc(console: var Console): int {. cdecl .}
+
+    CommandConcept* = concept x
+
+begin Command:
+    method execute(this: Command, console: var Console): int =
+        discard
 
 shape Command: @[
     Hook(
-        call: proc(app: var App): int =
-            let command = app.get(Command)
+        call: proc(console: var Console): int =
+            let command = console.app.get(Command)
 
             doAssert(
                 Command is CommandConcept,
                 "Failed to implement required fields and methods"
             )
 
-            result = command.execute(app)
+            result = command.execute(console)
     )
 ]
 
-#[
-## Console
-]#
-class Console:
-    var
-        app*: App
-        args*: seq[string]
-        opts*: Table[string, string]
-
-    method build*(app: var App): Console {. static .} =
-        result = Console.init(app)
-
-    method init*(app: var App): void =
+begin Console:
+    method init*(this: var Console, app: var App): void =
         this.app = app
 
-    method run*(): int =
+    method run*(this: var Console): int =
         result = 0
 
         for kind, key, val in getopt():
@@ -66,9 +58,12 @@ class Console:
             let command = this.app.config.findOne(Command, (name: this.args[0]))
 
             if command != nil:
-                result = cast[CommandHook](command.hook)(this.app)
+                result = cast[CommandHook](command.hook)(this)
+
+    proc build*(self: typedesc[Console], app: var App): Console =
+        result = Console.init(app)
 
 shape Console: @[
-#    Shared(),
+    Shared(),
     Delegate()
 ]
