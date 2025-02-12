0971517507d8e0c9712d0d6d5b2999616f210917
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2025-02-11 20:31:43 -0800

    Initial args/opts/help handling

--- src/mininim/cli.nim
+++ src/mininim/cli.nim
@@ -7,9 +7,23 @@ export
     dic # required since our hook calls `app.get`
 
 type
+    Arg* = object
+        name*: string
+        require*: bool = false
+        description*: string = "No description available"
+
+    Opt* = object
+        long*: string
+        short*: string
+        values*: seq[string]
+        require*: bool = false
+        description*: string = "No description available"
+
     Command* = ref object of Facet
         name*: string
         description*: string
+        args*: seq[Arg]
+        opts*: seq[Opt]
 
     Process* = ref object of Class
 
@@ -44,6 +58,98 @@ begin Console:
     method init*(app: App): void {. base, mutator .} =
         this.app = app
 
+    method help() {. base .} =
+        echo unindent(
+            fmt """
+
+                Welcome to mininim.
+
+                usage: mininim [command]
+
+                {"-h, --help".alignLeft(20)} Get help for the command
+
+                commands:
+            """
+        )
+
+        var
+            commands = this.app.config.findall(Command)
+
+        commands.sort(
+            proc(a: Command, b: Command): int =
+                result = cmp(a.name, b.name)
+        )
+
+        for command in commands:
+            echo fmt """{command.name.alignLeft(20)} {command.description}"""
+
+        echo ""
+
+    method help(command: Command): void {. base .} =
+        var
+            defs: seq[string]
+            optDef: string
+
+        for opt in command.opts:
+            optDef = ""
+
+            if opt.require:
+                if bool opt.short.len:
+                    optDef = fmt "-{opt.short}"
+                else:
+                    optDef = fmt "--{opt.long}"
+
+                if bool opt.values.len:
+                    optDef.add(":")
+                    optDef.add(opt.values[0])
+
+                defs.add(optDef)
+
+        for arg in command.args:
+            if arg.require:
+                defs.add(fmt "[{arg.name}]")
+
+        echo unindent(
+            fmt """
+
+                usage: mininim {command.name} {defs.join(" ")}
+
+            """
+        )
+
+        echo fmt """{"-h, --help".alignLeft(20)} Get help for the command"""
+
+        for opt in command.opts:
+            optDef = ""
+
+            if bool opt.short.len:
+                optDef.add(fmt "-{opt.short}")
+
+                if bool opt.long.len:
+                    optDef.add(", ")
+            else:
+                optDef.add("    ")
+
+            if bool opt.long.len:
+                optDef.add(fmt "--{opt.long}")
+
+            echo fmt """{optDef.alignLeft(20)} {opt.description}"""
+
+        echo ""
+
+    #[
+        Check whether or not an option was passed
+    ]#
+    method hasOpt(names: varargs[string]): bool {. base .} =
+        result = false
+
+        for name in names:
+            if this.opts.hasKey(name):
+                return true
+
+    #[
+        Run the console
+    ]#
     method run*(): int {. base .} =
         result = 0
 
@@ -56,12 +162,21 @@ begin Console:
                 of cmdEnd:
                     discard
 
-        if this.args.len > 0:
+        if bool this.args.len:
             let
                 command = this.app.config.findOne(Command, (name: this.args[0]))
 
             if command != nil:
-                result = cast[CommandHook](command.hook)(this)
+                if this.hasOpt("h", "help"):
+                    this.help(command)
+                else:
+                    result = cast[CommandHook](command.hook)(this)
+            else:
+                result = 1
+                echo fmt "Unknown command {this.args[0]}, use --help to list commands."
+        else:
+            this.help()
+
 
 shape Console: @[
     Shared(),
